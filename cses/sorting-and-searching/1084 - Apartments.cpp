13be0ef817cf65bc57db34c63112bef5b1969569
Partha Biswas <prtbws@zohomail.in>
2026-09-16 23:58:14 +0530

    Add Apartments (sorting-and-searching)

---
+++ cses/sorting-and-searching/1084 - Apartments.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m ,k;
+    cin >> n >> m >> k;
+
+    vector<int>a(n), b(m);
+
+    for (int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    for (int i=0; i<m; i++){
+        cin >> b[i];
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    int ans = 0;
+
+    int i= 0, j = 0;
+
+    while(i < n && j < m){
+
+        if (b[j] < a[i]-k) j++;
+        else if (b[j] > a[i]+k) i++;
+        else {
+            ans++;
+            i++;
+            j++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
