a2bef05fb935c217e1d6d08126393fd010dc1593
Partha Biswas <prtbws@zohomail.in>
2026-09-20 14:43:32 +0530

    Add Two Knights (introductory-problems)

---
+++ cses/introductory-problems/1072 - Two Knights.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    long long n;
+    cin >> n;
+
+    for (long long k = 1; k<=n; k++) {
+        cout << k*k*(k*k-1)/2 - 4*(k-1)*(k-2) << endl;
+    }
+
+    return 0;
+}
